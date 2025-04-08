5e5de8e4b0d6bda4ab06db9ec2bb8bf9124709ae
svyat <svyat@DESKTOP-P5UR6UF>
2025-04-08 18:34:24 +0300

    Добавьте файлы проекта.

---
+++ Lab1.2/Lab1.2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+
+class Entity {
+protected:
+    std::string name;
+    int health;
+
+public:
+    Entity(const std::string& n, int h) : name(n), health(h) {}
+
+    virtual void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health << std::endl;
+    }
+
+    virtual ~Entity() {}
+};
+
+class Player : public Entity {
+private:
+    int experience;
+
+public:
+    Player(const std::string& n, int h, int exp)
+        : Entity(n, h), experience(exp) {
+    }
+
+    void displayInfo() const override {
+        Entity::displayInfo();
+        std::cout << "Experience: " << experience << std::endl;
+    }
+};
+
+class Enemy : public Entity {
+private:
+    std::string type;
+
+public:
+    Enemy(const std::string& n, int h, const std::string& t)
+        : Entity(n, h), type(t) {
+    }
+
+    void displayInfo() const override {
+        Entity::displayInfo();
+        std::cout << "Type: " << type << std::endl;
+    }
+};
+
+
+class Boss : public Enemy {
+private:
+    std::string specialAbility;
+
+public:
+    Boss(const std::string& n, int h, const std::string& t, const std::string& ability)
+        : Enemy(n, h, t), specialAbility(ability) {
+    }
+
+    void displayInfo() const override {
+        Enemy::displayInfo();
+        std::cout << "Special Ability: " << specialAbility << std::endl;
+    }
+};
+
+int main() {
+    Player hero("Hero", 100, 0);
+    Enemy monster("Goblin", 50, "Goblin");
+
+    hero.displayInfo();
+    monster.displayInfo();
+
+    std::cout << "\n--- Testing Boss ---\n";
+
+    Boss boss("Ogre Magi", 200, "Ogre", "FireBlast");
+    boss.displayInfo();
+
+    return 0;
+}
